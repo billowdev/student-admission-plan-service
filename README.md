aeff2c079350714de303095aa230389e5271c9fa
Thanawat Joyjeed <89732874+thanawattui@users.noreply.github.com>
2023-02-23 16:02:05 +0700

    Add extra adminssion plan (#5)
    * add-thanawat
    
    * Update README.md (#1)
    
    * add-thanawat (#3)
    
    Co-authored-by: Akkarapon Phikulsri <lacakira@gmail.com>
    
    * Update README.md (#2)
    
    * extra-admission-plan-model
    
    ---------
    
    Co-authored-by: tamwannida <125627340+tamwannida@users.noreply.github.com>
    Co-authored-by: Akkarapon Phikulsri <lacakira@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # sapms-service
 
 
+
+
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { json } from 'body-parser';
 import userController from './user/controllers/user.controller';
-
+import extraAdmissionPlanController from "./extra-admission-plan/controllers/extra-admission-plan.controller";
 export const App = () =>{
 
 	const app = express()
@@ -10,6 +10,8 @@ export const App = () =>{
 	app.route("/users").get(userController.getAllUsers)
 	app.route("/users/login").post(userController.handleLogin)
 	app.route("/users/create").post(userController.handleCreateUser)
+
+	app.route("/extra-admission-plans").get(extraAdmissionPlanController.handleGetAll)
 		
 
 	app.get("/", (req, res) => {
