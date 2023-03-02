938faac2391baf00dc2e730d61878296ed8c70e2
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 09:50:55 +0700

    Create user.types.ts

---
+++ src/modules/user/types/user.types.ts
@@ -0,0 +1,33 @@
+export enum UserRole {
+	ADMIN = 'admin',
+	USER = 'user',
+}
+
+// export interface UserEnum
+export interface UserAttributes {
+	id?: string;
+	email: string;
+	username: string;
+	password: string;
+	name?: string;
+	surname?: string;
+	phone?: string;
+	role?: UserRole;
+	faculty?: string;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+export interface UserQueryInterface {
+	email?: string;
+	username?: string;
+	name?: string;
+	surname?: string;
+	phone?: string;
+	role?: UserRole;
+	faculty?: string;
+  }
+  
+export type LoginResponse = {
+	token: string;
+}
