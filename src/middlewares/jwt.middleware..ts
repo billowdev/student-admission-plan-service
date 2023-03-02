6b205f8b24f83eff5d39391dff20bf7b19408c4a
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 10:56:23 +0700

    Create jwt.middleware..ts

---
+++ src/middlewares/jwt.middleware..ts
@@ -0,0 +1,16 @@
+import { JWT_SECRET } from './../common/constants/common.constants';
+import jwt from 'jsonwebtoken'
+import { JWT_EXPRIES } from './../common/constants/common.constants';
+
+export const createJwtToken = (claims: object): string => {
+	const token: string = jwt.sign(
+		{
+			...claims
+		},
+		JWT_SECRET as string,
+		{
+			expiresIn: JWT_EXPRIES as string,
+		},
+	)
+	return token
+}
