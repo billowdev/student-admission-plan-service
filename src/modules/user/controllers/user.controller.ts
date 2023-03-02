298108fc6cb4d2e173bdec552708678a0f20b142
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 10:02:15 +0700

    Update user.controller.ts

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,41 +1,42 @@
 
 import { Request, Response, Express } from 'express';
-import { UserAttributes } from '../types/user.types';
 import userService from './../services/user.service';
-import { hashPassword, verifyPassword } from './../../../common/utils/password-hasher';
-import { AuthError, UserNotFoundException } from './../../../common/exceptions/user.exception';
-import { handleCreateAdmissionPlan } from './../../admission-plan/controllers/admission-plan.controller';
+import { AuthError, UserAlreadyExistsError, UserNotFoundException } from './../../../common/exceptions/user.exception';
 
 export const handleCreateUser = async (req: Request, res: Response): Promise<void> => {
-	const { username, password } = req.body;
-
 	try {
-		const user = await userService.createUser({ username, password });
+		const user = await userService.createUser(req.body);
 		res.status(201).json(user);
 	} catch (error) {
-		console.error(error);
-
-		res.status(500).json({ message: 'Internal server error' });
+		if (error instanceof UserAlreadyExistsError) {
+			res.status(400).json({ message: error.message });
+		} else {
+			console.error(error);
+			res.status(500).json({ message: 'Internal server error' });
+		}
 	}
 };
 
 
 export const handleLogin = async (req: Request, res: Response): Promise<void> => {
-	const { username, password } = req.body;
-
+	const { identifier, password } = req.body;
+  
 	try {
-		const { token } = await userService.login(username, password);
-		res.status(200).json({ token });
+	  const { token } = await userService.login(identifier, password);
+	  res.status(200).json({ token });
 	} catch (error) {
-		console.error(error);
-
-		if (error instanceof AuthError) {
-			res.status(401).json({ message: error.message });
-		} else {
-			res.status(500).json({ message: 'Internal server error' });
-		}
+	  console.error(error);
+  
+	  if (error instanceof AuthError) {
+		res.status(401).json({ message: error.message });
+	  } else if (error instanceof Error) {
+		res.status(500).json({ message: error.message });
+	  } else {
+		res.status(500).json({ message: 'Internal server error' });
+	  }
 	}
-};
+  };
+  
 
 export const handleDeleteUser = async (req: Request, res: Response): Promise<void> => {
 	const { id } = req.params;
@@ -71,6 +72,7 @@ export const handleUpdateUser = async (req: Request, res: Response): Promise<voi
 		}
 	}
 };
+
 
 
 
