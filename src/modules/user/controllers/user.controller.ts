d64e7c96ff492eed44054df61bdde319b7b77a22
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 09:50:49 +0700

    Update user.controller.ts

--- src/modules/user/controllers/user.controller.ts
+++ src/modules/user/controllers/user.controller.ts
@@ -1,31 +1,83 @@
 
 import { Request, Response, Express } from 'express';
+import { UserAttributes } from '../types/user.types';
 import userService from './../services/user.service';
+import { hashPassword, verifyPassword } from './../../../common/utils/password-hasher';
+import { AuthError, UserNotFoundException } from './../../../common/exceptions/user.exception';
+import { handleCreateAdmissionPlan } from './../../admission-plan/controllers/admission-plan.controller';
 
-export const handleGetAll = async (req: Request, res: Response) => {
-	const data = userService.handleGetAllUser()
-	res.json({ message: "get all", data });
-}
-
-export const handleLogin = async (req: Request, res: Response) => {
+export const handleCreateUser = async (req: Request, res: Response): Promise<void> => {
 	const { username, password } = req.body;
-	const response = await userService.login({ username, password })
-	res.json({ message: 'login success', response })
-}
 
-export const handleCreateUser = async (req: Request, res: Response) => {
 	try {
-		const { username, password } = req.body;
-		const user = await userService.createUser({username, password});
-		res.status(201).json({ user });
+		const user = await userService.createUser({ username, password });
+		res.status(201).json(user);
 	} catch (error) {
-		res.status(400).json({ error: 'can not create user' });
+		console.error(error);
+
+		res.status(500).json({ message: 'Internal server error' });
 	}
-}
+};
 
 
-export default {	
+export const handleLogin = async (req: Request, res: Response): Promise<void> => {
+	const { username, password } = req.body;
+
+	try {
+		const { token } = await userService.login(username, password);
+		res.status(200).json({ token });
+	} catch (error) {
+		console.error(error);
+
+		if (error instanceof AuthError) {
+			res.status(401).json({ message: error.message });
+		} else {
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	}
+};
+
+export const handleDeleteUser = async (req: Request, res: Response): Promise<void> => {
+	const { id } = req.params;
+
+	try {
+		await userService.deleteUser(id);
+		res.status(200).json({ message: `User with id ${id} has been deleted successfully` });
+	} catch (error) {
+		console.error(error);
+
+		if (error instanceof UserNotFoundException) {
+			res.status(404).json({ message: error.message });
+		} else {
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	}
+};
+
+export const handleUpdateUser = async (req: Request, res: Response): Promise<void> => {
+	const { id } = req.params;
+	const user = req.body;
+
+	try {
+		const updatedUser = await userService.updateUser(id, user);
+		res.status(200).json(updatedUser);
+	} catch (error) {
+		console.error(error);
+
+		if (error instanceof UserNotFoundException) {
+			res.status(404).json({ message: error.message });
+		} else {
+			res.status(500).json({ message: 'Internal server error' });
+		}
+	}
+};
+
+
+
+export default {
 	handleLogin,
 	handleCreateUser,
-	handleGetAll
+	handleDeleteUser,
+	handleUpdateUser
+
 }
