8b2621a1f74805e89c29ed210ad2d67613af5aba
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 13:09:10 +0700

    Create responsible-quota-person.types.ts

---
+++ src/modules/responsible-quota-person/types/responsible-quota-person.types.ts
@@ -0,0 +1,35 @@
+import { Query } from "express-serve-static-core";
+
+export enum QuotaType {
+	GOOD_STUDY = 'good_study',
+	GOOD_ACTIVITY = 'good_activity',
+	GOOD_SPORT = 'good_sport',
+	GOOD_PERSON = 'good_person',
+}
+
+
+export interface ResponsibleQuotaPersonAttributes {
+	id?: string;
+	year?: number;
+	name?: string;
+	surname?: string;
+	agency?: string;
+	phone?: string;
+	quota?: QuotaType;
+	createdAt?: Date;
+	updatedAt?: Date;
+}
+
+
+export interface ResponsibleQuotaPersonQueryInterface extends Query {
+	year?: string;
+	name?: string;
+	surname?: string;
+	agency?: string;
+	phone?: string;
+	quota?: string;
+}
+
+export interface ResponsibleQuotaPersonParamInterface {
+	id: string
+}
