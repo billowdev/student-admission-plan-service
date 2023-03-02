0acb199b2d58bced4bd03e86ab52fd7cf21e9358
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 09:50:46 +0700

    Update admission-plan.route.ts

--- src/modules/admission-plan/routes/admission-plan.route.ts
+++ src/modules/admission-plan/routes/admission-plan.route.ts
@@ -2,13 +2,14 @@ import express from 'express';
 import admissionPlanControllers from '../controllers/admission-plan.controller';
 import { validateAdmissionPlan, validateAdmissionPlanQueryParams } from '../middlewares/admission-plan.middleware';
 import { validateUUID } from './../../../middlewares/validate-uuid.common.middleware';
+import { authMiddleware } from '../../../middlewares/auth.middleware'; 
 
 const router = express.Router();
 
 router.get("/get_all", validateAdmissionPlanQueryParams, admissionPlanControllers.handleGetAllAdmissionPlan)
-router.get("/get_one/:id", validateUUID ,admissionPlanControllers.handleGetOneAdmissionPlan)
+router.get("/get_one/:id", validateUUID, admissionPlanControllers.handleGetOneAdmissionPlan)
 router.post("/create", validateAdmissionPlan, admissionPlanControllers.handleCreateAdmissionPlan)
 router.patch("/update/:id", admissionPlanControllers.handleUpdateAdmissionPlan)
-router.delete("/delete/:id", validateUUID, admissionPlanControllers.handleDeleteAdmissionPlan)
+router.delete("/delete/:id", validateUUID, authMiddleware, admissionPlanControllers.handleDeleteAdmissionPlan)
 
 export default router;
