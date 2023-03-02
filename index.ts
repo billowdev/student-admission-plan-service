ae675d784771b5f12502e72d66d1bef205199b6b
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-02 09:50:58 +0700

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,15 +1,23 @@
 import App from "./src/app"
 const app = App()
 import db from "./src/database/models"
+import dotenv from 'dotenv';
 
-db.sequelize.sync({ force: true }).then(() => {
-	app.listen(5000, () => {
-		console.log("SERVER ON 5000")
-		return "server"
-	})
-}).then(() => {
-	console.log('Database synced successfully');
-})
-	.catch((error: any) => {
-		console.error('Error syncing database:', error);
-	});
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await db.sequelize.sync({ force: true });
+    console.log('Database synced successfully');
+    
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Error syncing database:', error);
+  }
+}
+
+startServer();
