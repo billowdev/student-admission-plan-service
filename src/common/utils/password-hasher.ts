b4b47a27cc8d7cfc1f22dfda404671bdd76c594d
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-07 16:11:37 +0700

    change argon2 to bcrypt

--- src/common/utils/password-hasher.ts
+++ src/common/utils/password-hasher.ts
@@ -1,15 +1,18 @@
-import argon2 from 'argon2';
+// import argon2 from 'argon2';
+const bcrypt = require('bcrypt');
 
 export const hashPassword = async (password: string): Promise<string> => {
-  const hash = await argon2.hash(password, {
-    type: argon2.argon2id,
-  });
-
+  // const hash = await argon2.hash(password, {
+  //   type: argon2.argon2id,
+  // });
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
   return hash;
 };
 
 export const verifyPassword = async (password: string, hash: string): Promise<boolean> => {
-  const isMatch = await argon2.verify(hash, password);
-
+  // const isMatch = await argon2.verify(hash, password);
+  const isMatch = await bcrypt.compare(password, hash);
   return isMatch;
 };
