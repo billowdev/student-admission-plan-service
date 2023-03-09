b08bada72d47b3212d3132fa3b6925d0de28c7c7
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-09 23:50:46 +0700

    Update responsible-quota-person.route.ts

--- src/modules/responsible-quota-person/routes/responsible-quota-person.route.ts
+++ src/modules/responsible-quota-person/routes/responsible-quota-person.route.ts
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get("/get-all", responsibleQuotaPersonController.handleGetAllRQP)
-router.get("/get-one/:id", validateUUID, responsibleQuotaPersonController.handleGetOneRQP)
+router.get("/get-one/:id", responsibleQuotaPersonController.handleGetOneRQP)
 
 router.post("/create", responsibleQuotaPersonController.handleCreateRQP)
 router.patch("/update/:id", responsibleQuotaPersonController.handleUpdateRQP)
