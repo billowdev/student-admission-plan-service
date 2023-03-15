c1d1abd3986bb74aadac2371a7de22f6f8f061ee
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-15 23:58:09 +0700

    Update responsible-quota-person.route.ts

--- src/modules/responsible-quota-person/routes/responsible-quota-person.route.ts
+++ src/modules/responsible-quota-person/routes/responsible-quota-person.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { UserRole } from '../../../modules/user/types/user.types';
+import { authMiddleware, roleMiddleware } from '../../../middlewares/auth.middleware';
 import responsibleQuotaPersonController from '../controllers/responsible-quota-person.controller'
 import validateUUID from './../../../middlewares/validate-uuid.common.middleware';
 const router = express.Router();
@@ -7,8 +9,8 @@ const router = express.Router();
 router.get("/get-all", responsibleQuotaPersonController.handleGetAllRQP)
 router.get("/get-one/:id", responsibleQuotaPersonController.handleGetOneRQP)
 
-router.post("/create", responsibleQuotaPersonController.handleCreateRQP)
-router.patch("/update/:id", responsibleQuotaPersonController.handleUpdateRQP)
-router.delete("/delete/:id", validateUUID,responsibleQuotaPersonController.handleDeleteRQP)
+router.post("/create", authMiddleware, responsibleQuotaPersonController.handleCreateRQP)
+router.patch("/update/:id", authMiddleware, responsibleQuotaPersonController.handleUpdateRQP)
+router.delete("/delete/:id", authMiddleware, roleMiddleware(UserRole.ADMIN), validateUUID, responsibleQuotaPersonController.handleDeleteRQP)
 
 export default router;
