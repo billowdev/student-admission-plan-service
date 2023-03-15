82a7d53cd8bcded2981402ffa175eb70738ebc04
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-15 23:58:08 +0700

    Update extra-admission-plan.route.ts

--- src/modules/extra-admission-plan/routes/extra-admission-plan.route.ts
+++ src/modules/extra-admission-plan/routes/extra-admission-plan.route.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { authMiddleware, roleMiddleware } from '../../../middlewares/auth.middleware';
+import { UserRole } from '../../../modules/user/types/user.types';
 import extraAdminssionPlanControllers from '../controllers/extra-admission-plan.controller';
 import { validateCreateExtraAdmissionPlan, validateExtraAdmissonPlanQueryParams } from '../middlewares/extra-admission-plan.middleware';
 import { validateUUID } from './../../../middlewares/validate-uuid.common.middleware';
@@ -11,10 +13,9 @@ router.get("/get-by-faculty/:faculty", extraAdminssionPlanControllers.handleGetA
 router.get("/get-exists-year", extraAdminssionPlanControllers.handleGetYearListExtraAdmissionPlan)
 router.get("/get-exists-faculty", extraAdminssionPlanControllers.handleGetFacultyListExtraAdmissionPlan)
 
-
 router.get("/get-one/:id", validateUUID, extraAdminssionPlanControllers.handleGetOneExtraAdmissionPlan)
-router.post("/create", validateCreateExtraAdmissionPlan, extraAdminssionPlanControllers.handleCreateExtraAdmissionPlan)
-router.patch("/update/:id", validateUUID, extraAdminssionPlanControllers.handleUpdateExtraAdmissionPlan)
-router.delete("/delete/:id", validateUUID,extraAdminssionPlanControllers.handleDeleteExtraAdmissionPlan)
+router.post("/create", validateCreateExtraAdmissionPlan, authMiddleware, extraAdminssionPlanControllers.handleCreateExtraAdmissionPlan)
+router.patch("/update/:id", validateUUID, authMiddleware, extraAdminssionPlanControllers.handleUpdateExtraAdmissionPlan)
+router.delete("/delete/:id", validateUUID, authMiddleware, roleMiddleware(UserRole.ADMIN), extraAdminssionPlanControllers.handleDeleteExtraAdmissionPlan)
 
 export default router;
